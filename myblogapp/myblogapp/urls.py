d40e204b2452474f392cd20c7a459e7caf333002
Owner <a-okubo@krc.biglobe.ne.jp>
2020-11-15 01:01:54 +0900

    Lesson30対応。

--- myblogapp/myblogapp/urls.py
+++ myblogapp/myblogapp/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.urls import include, path
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
+    path('posts/', include('posts.urls')),
     path('admin/', admin.site.urls),
-    path('posts/', include('posts.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
